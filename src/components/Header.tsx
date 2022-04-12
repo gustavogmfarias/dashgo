a37badd38748c8990f3c597b4606abb959f99de3
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-04-11 23:37:11 -0300

    .

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Flex as="header" width="100%">
+      {" "}
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/Form/Input";
+
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" id="email" type="email" label="E-mail" />
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            label="Password"
+          />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
